4151f88db66477d923c1ff1d93baa995f55e9ae2
RioRustik <possession1931@gmail.com>
2021-09-24 17:26:45 +0300

    12

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" ..\UnitTest\bin\Debug\UnitTest.dll
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" UnitTest.dll
